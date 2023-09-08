d70e8f2769d6fd096f79b123830a72ff2b50be4c
Liam-Coulson <coulsonl2209@gmail.com>
2023-09-08 13:07:32 +0100

    Add image credit and license, finish project

---
+++ script.js
@@ -0,0 +1,12 @@
+const imageButton = document.querySelector(".image-credit");
+
+imageButton.addEventListener("click", function () {
+    alert(`All animal profile icons created by Freepik - Flaticon.\n
+    SVG icons from Pictogrammers.com, made by:\n
+    Colton Wiscombe\n
+    Michael Irigoyen\n
+    Simran\n
+    Austin Andrews\n
+    GreenTurtwig\n
+    Google`)
+})
